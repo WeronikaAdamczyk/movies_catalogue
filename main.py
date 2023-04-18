c80bd4b2716c3978110cf4cacc43415e8c61eea2
Weronika Adamczyk <weronika.mroziak@gmail.com>
2023-04-18 11:25:49 +0200

    updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import tmdb_client
 from tmdb_client import get_popular_movies
-from tmdb_client import get_poster_url
+from flask import request
 
 app = Flask(__name__)
 
@@ -11,6 +11,7 @@ app = Flask(__name__)
 #     response = get_popular_movies()
 #     movies=response["results"]
 #     return render_template("homepage.html", movies=movies)
+lists = ["now_playing","popular","upcoming","top_rated"]
 
 
 @app.context_processor
@@ -22,8 +23,20 @@ def utility_processor():
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(how_many=8)
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get('list_type', 'popular')
+    if not lists.__contains__(selected_list):
+        selected_list='popular'
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list,lists=lists)
+
+
+
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+   details = tmdb_client.get_single_movie(movie_id)
+   cast = tmdb_client.get_single_movie_cast(movie_id)
+   return render_template("movie_details.html", movie=details, cast=cast)
 
 
 if __name__ == '__main__':
